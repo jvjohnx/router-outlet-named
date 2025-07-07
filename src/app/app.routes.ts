42e4e7ac6e4c7d70d3432c9eb5dffaef4bc2d9f4
johnson <johnson@jvjohn.org>
2025-07-06 20:10:13 -0400

    push code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home.component';
+import {ChatComponent} from './chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: "full"
+
+  },
+  {
+    path: 'chat',
+    component: ChatComponent,
+    outlet: 'sidebar',
+  }
+];
---
+++ src/app/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-home',
+  template: `<h2>Home Component</h2><p>This is the main view.</p>`
+})
+export class HomeComponent { }
